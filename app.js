4b9a04d362ab5b407015db1c129f903948ba4d0c
Snehashis Ratna <snehashisr10@gmail.com>
2023-04-23 14:58:03 +0530

    import javascript

---
+++ app.js
@@ -0,0 +1,8 @@
+// user Input
+let name = prompt("Give me your userName :");
+
+// Processing
+let welcomeMeassage  =  "This Script Works Properly " + name +"?" 
+
+// Output
+alert(welcomeMeassage) ;
