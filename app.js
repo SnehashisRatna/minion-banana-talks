63d1b1b60e7f0db2f9702e197e54842fd956d019
Snehashis Ratna <snehashisr10@gmail.com>
2023-04-24 23:38:36 +0530

    update : calling server for processing

--- app.js
+++ app.js
@@ -2,10 +2,21 @@ let btnTranslate = document.querySelector("#btn-translate") ;
 let txtInput = document.querySelector("#txt-input");
 let outputDiv = document.querySelector("#output");
 
+var serverURL = "https://lessonfourapi.tanaypratap.repl.co/translate/yoda.json"
 
+function getTranslationURL(text) {
+   return serverURL + "text=" + text 
+}
 
 function clickEventHandler(){
-   outputDiv.innerText = "asdasfsfa" + txtInput.value ; 
+   // outputDiv.innerText = "asdasfsfa" + txtInput.value ; 
+   var inputText = txtInput.value ; //talking input
+
+   //calling server for processing
+   fetch(getTranslationURL(inputText))
+    .then(Response => Response.json())
+    .then(json => console.log(json.console.transalted))
+
 };
 
 btnTranslate.addEventListener("click" ,  clickEventHandler) ;
